67f5604b2a95869aabe86f7eb83a5102960c76ea
Adrian <alextravc.342@gmail.com>
2025-08-11 23:48:37 -0500

    feat: MovieHorizontalList

---
+++ presentation/movies/components/MovieHorizontalList.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FlatList, Text, View } from 'react-native';
+
+import { Movie } from '@/infraestructure/interfaces/movie.interface';
+import MoviePoster from './MoviePoster';
+
+interface MovieHorizontalListProps {
+  movies: Movie[];
+  title?: string;
+}
+
+const MovieHorizontalList = ({ movies, title }: MovieHorizontalListProps) => {
+  return (
+    <View>
+      {title && <Text className="text-3xl font-bold px-4 mb-2">{title}</Text>}
+
+      <FlatList
+        horizontal
+        data={movies}
+        renderItem={({ item }) => (
+          <MoviePoster id={item.id} poster={item.poster} smallPoster />
+        )}
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+export default MovieHorizontalList;

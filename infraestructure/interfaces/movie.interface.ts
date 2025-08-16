5e8820d824b43d58e0bde2506953513723ca5f81
Adrian <alextravc.342@gmail.com>
2025-08-15 20:48:05 -0500

    feat: CompleteMovie

--- infraestructure/interfaces/movie.interface.ts
+++ infraestructure/interfaces/movie.interface.ts
@@ -7,3 +7,12 @@ export interface Movie {
   poster: string;
   backdrop: string;
 }
+
+export interface CompleteMovie extends Movie {
+  budget: number;
+  revenue: number;
+  runtime: number;
+  genres: string[];
+  productionCompanies: string[];
+  spokenLanguages: string[];
+}

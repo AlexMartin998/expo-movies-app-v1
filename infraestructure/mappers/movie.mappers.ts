0131987eaf0b72cfc16dcf23b201ac924311ad48
Adrian <alextravc.342@gmail.com>
2025-08-15 20:48:21 -0500

    feat: fromTheMovieDBToCompleteMovie

--- infraestructure/mappers/movie.mappers.ts
+++ infraestructure/mappers/movie.mappers.ts
@@ -1,4 +1,5 @@
-import { Movie } from '../interfaces/movie.interface';
+import { CompleteMovie, Movie } from '../interfaces/movie.interface';
+import { MovieDBMoviewResponse } from '../interfaces/moviedb-movie-response';
 import { Result } from '../interfaces/moviedb-response';
 
 export class MovieMapper {
@@ -11,6 +12,29 @@ export class MovieMapper {
       rating: movieDB.vote_average,
       poster: `https://image.tmdb.org/t/p/w500${movieDB.poster_path}`,
       backdrop: `https://image.tmdb.org/t/p/w500${movieDB.backdrop_path}`,
+    };
+  }
+
+  static fromTheMovieDBToCompleteMovie(
+    movieDB: MovieDBMoviewResponse
+  ): CompleteMovie {
+    return {
+      id: movieDB.id,
+      title: movieDB.title,
+      description: movieDB.overview,
+      releaseDate: new Date(movieDB.release_date),
+      rating: movieDB.vote_average,
+      poster: `https://image.tmdb.org/t/p/w500${movieDB.poster_path}`,
+      backdrop: `https://image.tmdb.org/t/p/w500${movieDB.backdrop_path}`,
+
+      budget: movieDB.budget,
+      revenue: movieDB.revenue,
+      runtime: movieDB.runtime,
+      genres: movieDB.genres.map(genre => genre.name),
+      productionCompanies: movieDB.production_companies.map(
+        company => company.name
+      ),
+      spokenLanguages: movieDB.spoken_languages.map(language => language.name),
     };
   }
 }
